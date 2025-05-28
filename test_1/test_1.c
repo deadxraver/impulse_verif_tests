a929f8ddaeb55a8473f49ec3fb4c212b69871576
deadxraver <409853@edu.itmo.ru>
2025-05-28 10:40:05 +0300

    fix test_1, should have returned zero value on success, not 1

--- test_1/test_1.c
+++ test_1/test_1.c
@@ -14,15 +14,15 @@ int main(int argc, char* argv[])
 	int c = 0x8;
 	int d;
 	int e;
-	int ret = 0;
+	int ret = 1;
 
 	d = MAX(L_SHIFT(b, 0), R_SHIFT(a, 0));
 	e = MIN(L_SHIFT(b, 1), R_SHIFT(c, 1));
 
 	if(d != e)
-		ret = 1;
+		ret = 0;
 
-	printf("Test %s\n", ret ? "Passed": "Failed");
+	printf("Test %s\n", ret == 0 ? "Passed": "Failed");
 
 	return ret;
 }
