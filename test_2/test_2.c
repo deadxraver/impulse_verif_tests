192e653410d97727ed38ac853991bf95df9e7320
deadxraver <409853@edu.itmo.ru>
2025-05-28 11:01:41 +0300

    I guess the problem was with wrong indexing (increment after getting the value, which means the programm would compare the first value with itself) + in first printf all values were printed in hd format except one

--- test_2/test_2.c
+++ test_2/test_2.c
@@ -4,7 +4,7 @@
 int main(int argc, char* argv[])
 {   long x[3] = {12345, 54321, 112233};
     printf("Hi, print here some infos!\n");
-    printf("Strange: %hd %d %hd\n", x[0], x[1], x[2]);
+    printf("Strange: %hd %hd %hd\n", x[0], x[1], x[2]);
     printf("But: %d %d %d\n", x[0], x[1], x[2]);
 // hex dump
     int i; char *p = (char *)&x;
@@ -13,9 +13,10 @@ int main(int argc, char* argv[])
     puts("");
 // some various checks
     long *lp = (long *)&x;
-    if (*lp > *(lp++)) puts("0 > 1");
+    if (*lp > *(++lp)) puts("0 > 1");
     else puts("1 > 0");
-    lp++; if (*lp > *(--lp)) puts("2 > 1");
+    lp++;
+    if (*lp > *(--lp)) puts("2 > 1");
     else puts("1 > 2");
     return 0;
 }
